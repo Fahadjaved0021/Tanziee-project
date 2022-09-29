323f5e6517ea0ac2af44b41378090fda21127ccb
Fahadjaved0021 <fahadjaved0021@gmail.com>
2022-09-29 22:28:29 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
 import { useState } from 'react'
 import React from 'react'
 import './App.css'
+import Die from './Component/Die'
+import {nanoid} from 'nanoid'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dice, setDice] = useState(allNewArray())
+
+  const dieElements = dice.map(die => <Die value={die.value} />) 
+
+  function allNewArray () {
+      const newDice = []
+      for (let i = 0; i < 10; i++) {
+        newDice.push({
+          
+        value: Math.ceil(Math.random()* 6), 
+        isHeld:false,
+        id: nanoid()
+      
+      })
+      }
+      return newDice
+  }
+
+
+  function rollDice () {
+    setDice(allNewArray)
+
+  }
+
+  console.log(allNewArray())
 
   return (
     <div className="App">
-      <p>helo there</p>
+      <main>
+        <div className='dice-container'>
+      {dieElements}
+      </div>
+
+      <button className='roll-dice' onClick={rollDice}>Roll</button>
+      
+      </main>
+      
     </div>
   )
 }
